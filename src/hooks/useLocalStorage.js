2ebd98ebbb019a06bd3742556720f8242d7e54ba
Denvinn Magsino <lambda_school_loaner_170@Lambda-School-Loaner-170s-MacBook-Pro.local>
2019-09-10 19:48:41 -0700

    finsihed mvp

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 
-export default const useLocalStorage = ( key, initialValue ) => {
+export const useLocalStorage = ( key, initialValue ) => {
+    const item = window.localStorage.getItem(key);
+
 
     const [ storedValue, setStoredValue ] = useState(()=>{
-        
-        const item = window.localStorage.getItem(key)
-
-        return item ? JSON.parse(item) : initialValue;
-
+        const item = window.localStorage.getItem(key);
+        return (item ? JSON.parse(item) : initialValue);
     });
 
-    const setValue = value =>{
-        
-        setStoredValue(value);
-
+    const setValue = value => {
         window.localStorage.setItem(key, JSON.stringify(value));
+        setStoredValue(value);
     };
 
 
-    return [storedValue]
+    return [storedValue, setValue];
 };
